2a2e1f64b48da8e186c7d1b78e2a6c81ef747a8b
Matt Cowley <me@mattcowley.co.uk>
2024-08-12 03:28:37 +0100

    Allow commands to run in user-installed context (#89)
    * Update discord-api-types + workers-discord
    
    * Allow user-install for all commands
    
    * Add notes to README for user-install commands
    
    * Improve command registration debugging
    
    * Add new invite for user-install
    
    * Don't scope staging to a specific guild

--- src/commands/invite.js
+++ src/commands/invite.js
@@ -1,24 +1,36 @@
-import { ComponentType, InteractionResponseType } from 'discord-api-types/payloads';
+import { ComponentType, InteractionResponseType, ApplicationIntegrationType, InteractionContextType } from 'discord-api-types/payloads';
 
 import { createEmbed } from '../utils/embed.js';
 import invite from '../components/invite.js';
+import inviteUser from '../components/invite-user.js';
 
 export default {
     name: 'invite',
     description: 'Get a link to add DNS over Discord to your server',
+    contexts: {
+        installation: [
+            ApplicationIntegrationType.GuildInstall,
+            ApplicationIntegrationType.UserInstall,
+        ],
+        interaction: [
+            InteractionContextType.Guild,
+            InteractionContextType.BotDM,
+            InteractionContextType.PrivateChannel,
+        ],
+    },
     execute: ({ response }) => response({
         type: InteractionResponseType.ChannelMessageWithSource,
         data: {
             embeds: [
                 createEmbed(
                     'Invite',
-                    'Invite DNS over Discord to your server with https://dns-over-discord.v4.wtf/invite',
+                    'Invite DNS over Discord to your server with https://dns-over-discord.v4.wtf/invite\n\nOr, use the bot anywhere by adding it to your account with https://dns-over-discord.v4.wtf/invite/user',
                 ),
             ],
             components: [
                 {
                     type: ComponentType.ActionRow,
-                    components: [ invite.component ],
+                    components: [ invite.component, inviteUser.component ],
                 },
             ],
         },
